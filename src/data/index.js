dbb7bb8443cca8dda1e093420d29deb453e31c9e
Mohammad Amin Sameti <mamins1376@gmail.com>
2017-09-02 19:10:48 +0430

    Fix ES6 problem with dir-loader

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import generatePosts from '@/posts';
-import directory from 'babel-loader!dir-loader!@/data/posts';
+import directory from 'dir-loader-es6!@/data/posts';
 
 Vue.use(Vuex);
 
